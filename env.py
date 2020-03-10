dd023992e62bb8f4d9b8d9f7c351f3293ad2ad52
djjtyn <djjtynan@gmail.com>
2020-03-10 15:51:39 +0000

    images uploading to s3

--- env.py
+++ env.py
@@ -3,3 +3,5 @@ os.environ.setdefault('SECRET_KEY', '8)g4k3^e^4pv64-#+e!6lyp^lif3c6#@bm8%y-+hx4*
 os.environ.setdefault('STRIPE_PUBLISHABLE', 'pk_test_hS6bA5ILNRPXhcLD2G7K5fla00z1Ld3RiU')
 os.environ.setdefault('STRIPE_SECRET', 'sk_test_8sPZuehLodpLjMGeNjoBKmbJ00pmRJnuyj')
 os.environ.setdefault('DATABASE_URL', 'postgres://kdmodmcgdksafj:8b433a0afaf853031c7f86e4a0a2f7a18d751c02f8a227c05f82c8fa029f4745@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dbueohbfq5466q')
+os.environ.setdefault('AWS_SECRET_KEY_ID', 'AKIASPTQTQD5RBO7UHNQ')
+os.environ.setdefault('AWS_SECRET_ACCESS_KEY', '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')
